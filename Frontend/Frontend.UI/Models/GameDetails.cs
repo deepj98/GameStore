4531c8fafd2bb9cbe84d5dffa2bffbc8076db753
Deep shah <deepjshah.1998@gmail.com>
2024-09-23 02:14:29 -0400

    Validation added to input fields

--- Frontend/Frontend.UI/Models/GameDetails.cs
+++ Frontend/Frontend.UI/Models/GameDetails.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Frontend.UI.Models;
 
@@ -7,10 +8,14 @@ public class GameDetails
 
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Name cannot be empty")]
+    [StringLength(50)]
     public required string Name { get; set; }
 
+    [Required(ErrorMessage ="Selet a Genre from the list, Genre cannot be Null")]
     public  string? GenreId { get; set; }
 
+    [Range(1.00,100.00,ErrorMessage = "Value cannot be less the 1 or exceed 100")]
     public decimal Price { get; set; }
 
     public DateOnly ReleaseDate { get; set; }
--- Frontend/Frontend.UI/Program.cs
+++ Frontend/Frontend.UI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents();
 builder.Services.AddSingleton<GamesClients>();
+builder.Services.AddSingleton<GenresClient>();
 
 var app = builder.Build();
 
